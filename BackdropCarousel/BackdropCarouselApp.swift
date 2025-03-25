bc572ca8534c34a8434ef33be152e3aab318aa5a
parisollie <pjff18@gmail.com>
2025-03-25 13:15:16 -0600

    Initial Commit

---
+++ BackdropCarousel/BackdropCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackdropCarouselApp.swift
+//  BackdropCarousel
+//
+//  Created by Paul F on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BackdropCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackdropCarousel/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  BackdropCarousel
+//
+//  Created by Paul F on 25/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+            .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
